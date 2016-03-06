982ac0121cd296284868d18c7276aebd39290817
Prasad Jayashanka <prasadjayashanka@gmail.com>
2016-03-06 16:06:32 +0530

    working app engine upload redirector

--- uploader.php
+++ uploader.php
@@ -1,13 +1,19 @@
 <?php
 class uploader {
+
+		private function About(){
+			$arr = array('Name' => "Duo World Application Interface", 'Version' => "1.0.0-a", 'Change Log' => "Nothing so far just testing. Move along!", 'Author' => "Duo Software", 'Website' => "http://www.duoworld.com/", 'Status' => "Running");
+			echo json_encode($arr);
+		}
+
 		private function status(){
-			$arr = array('Name' => "Service Console Uploader Service", 'Version' => "1.0.0-a", 'Change Log' => "Nothing So Far! Testng!", 'Author' => "Duo Software", 'Repository' => "https://github.com/DuoSoftware/ServiceConsole/");
+			$arr = array('Name' => "Service Console Uploader Service", 'Version' => "1.0.0-a", 'Change Log' => "AppEngine Compatible uploader redirector.", 'Author' => "Duo Software", 'Repository' => "https://github.com/DuoSoftware/service_console_services");
 			echo json_encode($arr);
 		}
 
 		private function upload(){
-			ConsoleLog("uploadeeeeeeee");
-			$status = $this->uploadToObjectstore();
+			//$status = $this->uploadToObjectstore();
+			$status = $this->uploadWithoutCurl();
 			if ($status == "true"){
 				$this->addToQueue();
 			}else{
@@ -19,14 +25,15 @@ class uploader {
 
 			$url = SVC_OS_URL.SVC_UPLOAD_PATH.$_FILES['file']['name'];
 			ConsoleLog($url);
-		    $fname = $_FILES['file']['name'];  
-		    ConsoleLog("0.0"); 
-	    	$file = new CURLFile(realpath($_FILES['file']['tmp_name']));
-ConsoleLog("0.1");
+
+		    $fname = $_FILES['file']['name'];
+
+ 	    	$file = new CURLFile(realpath($_FILES['file']['tmp_name']));
+ 			var_dump($file);
 	        $post = array (
 	                  'file' => $file
 	                  );    
-ConsoleLog("0.5");
+
 		    $ch = curl_init();
 		    curl_setopt($ch, CURLOPT_URL, $url);
 		    curl_setopt($ch, CURLOPT_POST, 1);
@@ -38,10 +45,8 @@ ConsoleLog("0.5");
 		    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);  
 		    curl_setopt($ch, CURLOPT_TIMEOUT, 100);
 		    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		    ConsoleLog("1");
-		    $result = curl_exec ($ch);
+		    $result = curl_exec($ch);
 			curl_close ($ch);
-ConsoleLog("2");
 			$status = "false";
 
 		    if ($result === FALSE) {
@@ -55,16 +60,65 @@ ConsoleLog("2");
 		    return $status;
 		}
 
+		private function uploadWithoutCurl(){
+			ConsoleLog("huehueheu");
+			$file=file($_FILES['file']['tmp_name']);
+			$url = SVC_OS_URL.SVC_UPLOAD_PATH.$_FILES['file']['name'];
+			$files['file'] = $_FILES['file']; 
+			ConsoleLog($url);
+			$this->aaa($url, $files); 
+			return "true";
+
+		}
+
+		private function aaa($url, $files){ 
+		    $data = ""; 
+		    $boundary = "---------------------".substr(md5(rand(0,32000)), 0, 10); 
+
+		    $data .= "--$boundary\n"; 
+
+		    //Collect Filedata 
+		    foreach($files as $key => $file) 
+		    { 
+		        $fileContents = file_get_contents($file['tmp_name']); 
+
+		        $data .= "Content-Disposition: form-data; name=\"{$key}\"; filename=\"{$file['name']}\"\n"; 
+		        $data .= "Content-Type: multipart/form-data\n";
+		        $data .= "Content-Transfer-Encoding: binary\n\n"; 
+		        $data .= $fileContents."\n"; 
+		        $data .= "--$boundary--\n"; 
+		    } 
+
+		    $params = array('http' => array( 
+		           'method' => 'POST', 
+		           'header' => 'Content-Type: multipart/form-data; boundary='.$boundary, 
+		           'content' => $data 
+		        )); 
+
+		   $ctx = stream_context_create($params); 
+		   $fp = fopen($url, 'rb', false, $ctx); 
+
+		   if (!$fp) { 
+		     ConsoleLog("Problem with url"); 
+		   } 
+
+		   $response = @stream_get_contents($fp); 
+		   if ($response === false) { 
+		      ConsoleLog("Problem reading data from url"); 
+		   }else{
+		   		ConsoleLog("yay");
+		   }  
+		} 
+		
+
 		private function addToQueue(){
 			ConsoleLog("Starting Adding to Queue!");
 		}
 		
 		function __construct(){
-			ConsoleLog("executing constructor!");
+			Flight::route("GET /", function (){$this->About();});
 			Flight::route("GET /uploader", function (){$this->status();});
 		    Flight::route("POST /uploader/upload", function (){$this->upload();});
 		}
 	}
-
-
 ?>
