2ee4767872242aecdf38a98b5febc2a0d942a5cc
Prasad Jayashanka <prasadjayashanka@gmail.com>
2016-08-16 11:35:02 +0530

    chagned to cloudcharge objectstore

--- include/config.php
+++ include/config.php
@@ -1,11 +1,11 @@
 <?php
 //service console global variables
-define("SVC_OS_URL","http://developer.duoworld.com:3000");
-define("SVC_WORKER_URL","http://developer.duoworld.com:7500/");
-define("SVC_AUTH_URL","http://auth.production.duoworld.com:3048/");
-define("SVC_CEB_URL","http://developer.duoworld.com:3500/");
+define("SVC_OS_URL","http://dev.cloudcharge.com:3000");
+define("SVC_WORKER_URL","http://dev.cloudcharge.com:7500/");
+define("SVC_AUTH_URL","http://dev.cloudcharge.com:3048/");
+define("SVC_CEB_URL","http://dev.cloudcharge.com:3500/");
 define("SVC_UPLOAD_PATH","tasks.serviceconsole.payload/uploads/");
 define("SVC_AUTH_USERNAME","cookie@duosoftware.com");
 define("SVC_AUTH_PASSWORD","123");
-define("SVC_AUTH_DOMAIN","duoworld.com");
+define("SVC_AUTH_DOMAIN","dev.cloudcharge.com");
 ?>
